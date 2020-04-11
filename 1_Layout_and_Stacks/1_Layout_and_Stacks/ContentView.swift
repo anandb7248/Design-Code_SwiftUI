856b148801decefffca42dd5cc2162dff631b652
anandb7248 <anandb7248@gmail.com>
2020-04-10 21:38:46 -0700

    Components & Effects part 1

--- 1_Layout_and_Stacks/1_Layout_and_Stacks/ContentView.swift
+++ 1_Layout_and_Stacks/1_Layout_and_Stacks/ContentView.swift
@@ -11,36 +11,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            VStack {
-                Spacer()
-            }.frame(width: 300.0, height: 220.0)
-            .background(Color.blue)
-            .cornerRadius(20)
-            .shadow(radius: 40).offset(x: 0, y: -20)
+            BackCardView()
+                .background(Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 40)
+                .offset(x: 0, y: -40)
+                .scaleEffect(0.9)
+                .rotationEffect(Angle(degrees: 10))
+                .rotation3DEffect(.degrees(10), axis: (x: 10.0, y: 0, z: 0)).blendMode(.hardLight)
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.largeTitle)
-                            .foregroundColor(.white)
-                        Text("Certificate")
-                            .font(.headline)
-                            .foregroundColor(Color("accent"))
-                    }
-                    Spacer()
-                    Image("Logo1")
-                }
-                .padding()
-                Spacer()
-                Image("Card1")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill).frame(width: 300, height: 120, alignment: .top)
-            }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+            BackCardView()
+                .background(Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 40)
+                .offset(x: 0, y: -20)
+                .scaleEffect(0.95)
+                .rotationEffect(Angle(degrees: 5))
+                .rotation3DEffect(.degrees(5), axis: (x: 10.0, y: 0, z: 0)).blendMode(.hardLight)
+            
+            CardView().blendMode(.hardLight)
         }
     }
 }
@@ -48,5 +37,41 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .font(.headline)
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding()
+            Spacer()
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fill).frame(width: 300, height: 120, alignment: .top)
+        }
+        .frame(width: 340.0, height: 220.0)
+        .background(Color.black)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }.frame(width: 340.0, height: 220.0)
     }
 }
